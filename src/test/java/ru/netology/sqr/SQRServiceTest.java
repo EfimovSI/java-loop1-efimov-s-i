d03c55566bc4f25c26149dca3eea22497c4ac9f4
Sergey Efimov <yefimovsergey00@gmail.com>
2021-12-12 20:52:23 +0300

    Initial commit

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"'sqr is in range, boundaries not included', 110, 150, 2",
+            "'sqr is in range, boundaries included', 100, 289, 8",
+            "sqr is out of range, 1, 99, 0"})
+
+    void shouldCount(String name, int min, int max, int expected) {
+        SQRService counter = new SQRService();
+        int actual = counter.count(min, max);
+        assertEquals(expected, actual);
+    }
+}
